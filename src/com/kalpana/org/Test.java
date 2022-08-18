9037fc2cb19ee814ed3f38548f7a1217de710150
kalpana <venkatraman.kalpana@gmail.com>
2022-08-18 11:42:31 +0530

    first commit

---
+++ src/com/kalpana/org/Test.java
@@ -0,0 +1,14 @@
+package com.kalpana.org;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Student s=new Student("", 1, 4);
+	List<Student> l1=new LinkedList<Student>();
+
+	}
+
+}
---
+++ src/com/kalpana/org/Test2.java
@@ -0,0 +1,14 @@
+package com.kalpana.org;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String st1=new String("hello");
+		String str2=new String("hello");
+		System.out.println(st1.compareTo(str2));
+		//System.out.println(str2);
+		
+
+	}
+
+}
